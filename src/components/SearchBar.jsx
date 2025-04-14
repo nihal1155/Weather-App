1e065954e03217a86c24e645348399fa54244a8f
nihal1155 <shrivastavanihal95@gmail.com>
2025-04-14 16:44:19 +0000

    Moved API key to env

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import WeatherCard from "./WeatherCard";
 import Loader from "./Loader";
 import axios from "axios";
 
-const API_KEY = "UV32AAK8J4R43VCX6HF6BCCTV";
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const WEATHER_API_URL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
